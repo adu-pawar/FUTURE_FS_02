5513a26e21e3bac4ecc81e565b3be0280123f99b
adu-pawar <adu.pawar.1@gmail.com>
2026-05-09 17:26:51 +0530

    Update routing: Secure root path and redirect to login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,11 +16,9 @@ function App() {
       
       {/* Layout wraps all dashboard/CRM pages */}
       <Route element={<Layout />}>
-        {/* Public dashboard */}
-        <Route path="/" element={<Dashboard />} />
-        
         {/* Protected routes that require authentication */}
         <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Dashboard />} />
           <Route path="/customers" element={<CustomersListing />} />
           <Route path="/customers/add" element={<AddCustomer />} />
           <Route path="/customers/:id" element={<CustomerDetails />} />
