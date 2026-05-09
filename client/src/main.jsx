67b5252b95d22daf821b4b18f38d147d281c078b
adu-pawar <adu.pawar.1@gmail.com>
2026-05-09 19:10:29 +0530

    Fix router basename for GitHub Pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <App />
         <ToastContainer position="top-right" autoClose={3000} />
